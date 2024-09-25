cbfa6718a8f5f618f76036195401ff2bea2b6536
HAE2NI <hae0_02ni@ewhain.net>
2024-09-25 20:22:09 +0900

    💄 design: fix phone header svg

--- ceos/src/components/PhoneHeader.tsx
+++ ceos/src/components/PhoneHeader.tsx
@@ -1,17 +1,22 @@
 import styled from "styled-components";
 
-import BatteryIC from "@assets/icons/phbattery_ic.svg?react";
-import SignalIC from "@assets/icons/phsignal_ic.svg";
-import WifiIC from "@assets/icons/phwifi_ic.svg";
+import BatteryIC from "@assets/statusBar/phbattery_ic.svg?react";
+import SignalIC from "@assets/statusBar/phsignal_ic.svg?react";
+import WifiIC from "@assets/statusBar/phwifi_ic.svg?react";
+import TimeIC from "@assets/statusBar/phTime.svg?react";
 import { rowFlex } from "style/commonStyle";
 
 export default function PhoneHeader() {
   return (
     <Wrapper>
       <ContentWrapper>
-        <SignalIC />
-        <WifiIC />
-        <BatteryIC />
+        <TimeIcon />
+
+        <RightWrapper>
+          <SignalICon />
+          <WifiIcon />
+          <BatteryICon />
+        </RightWrapper>
       </ContentWrapper>
     </Wrapper>
   );
@@ -23,9 +28,34 @@ const Wrapper = styled.div`
   height: 48px;
 `;
 
+const RightWrapper = styled.div`
+  ${rowFlex}
+  gap: 5px;
+`;
+
 const ContentWrapper = styled.div`
   ${rowFlex}
   justify-content: space-between;
   width: 324.8px;
   height: 17px;
+`;
+
+const BatteryICon = styled(BatteryIC)`
+  width: 27px;
+  height: 13px;
+`;
+
+const WifiIcon = styled(WifiIC)`
+  width: 17px;
+  height: 13px;
+`;
+
+const SignalICon = styled(SignalIC)`
+  width: 20px;
+  height: 12px;
+`;
+
+const TimeIcon = styled(TimeIC)`
+  width: 31.5px;
+  height: 21px;
 `;
