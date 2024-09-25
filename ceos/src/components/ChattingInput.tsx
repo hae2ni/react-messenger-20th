03078ddb14a2833c606c3053a07710b36b2c6826
HAE2NI <hae0_02ni@ewhain.net>
2024-09-25 16:55:47 +0900

    💄 design: chattingInput UI

---
+++ ceos/src/components/ChattingInput.tsx
@@ -0,0 +1,35 @@
+import { ContentContainer, Icon, rowFlex } from "style/commonStyle";
+import styled from "styled-components";
+
+import PlusIC from "@/assets/icons/plus_ic.svg?react";
+import SmileIC from "@/assets/icons/smile_ic.svg?react";
+import HashtagIC from "@/assets/icons/hashtag_ic.svg?react";
+
+export default function ChattingInput() {
+  return (
+    <Wrapper>
+      <ContentContainer>
+        <PlusIcon />
+        <RightWrapper>
+          <SmileICon />
+          <HashtagICon />
+        </RightWrapper>
+      </ContentContainer>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  ${rowFlex}
+  width: 100%;
+  height: 56px;
+`;
+
+const RightWrapper = styled.div`
+  ${rowFlex}
+  gap: 16px;
+`;
+
+const PlusIcon = Icon(PlusIC);
+const SmileICon = Icon(SmileIC);
+const HashtagICon = Icon(HashtagIC);
