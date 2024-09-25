25f0a07ba12ba3510dac8299aacd4db25613332a
HAE2NI <hae0_02ni@ewhain.net>
2024-09-25 16:56:57 +0900

    💄 desgin: chattingRoom UI

--- ceos/src/pages/ChattingRoom.tsx
+++ ceos/src/pages/ChattingRoom.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import ChattingInput from "@components/ChattingInput";
+import ChattingRoomHeader from "@components/ChattingRoomHeader";
+import Layout from "style/layout/Layout";
+import styled from "styled-components";
 
 export default function ChattingRoom() {
-  return <div>chattingRoom</div>;
+  return (
+    <Layout>
+      <ChattingMain>
+        <ChattingRoomHeader />
+        <ChattingInput />
+      </ChattingMain>
+    </Layout>
+  );
 }
+
+const ChattingMain = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 730px;
+`;
