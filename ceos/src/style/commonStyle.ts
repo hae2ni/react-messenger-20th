64e998bef041a6eb7b6b0765196e6dce89939c85
HAE2NI <hae0_02ni@ewhain.net>
2024-09-25 15:53:35 +0900

    💄 design: commonStyle 추가

--- ceos/src/style/commonStyle.ts
+++ ceos/src/style/commonStyle.ts
@@ -1,4 +1,4 @@
-import { css } from "styled-components";
+import styled, { css } from "styled-components";
 
 export const rowFlex = css`
   display: flex;
@@ -10,4 +10,10 @@ export const columnFlex = css`
   ${rowFlex}
 
   flex-direction:column
+`;
+
+export const Icon = styled.button`
+  cursor: pointer;
+  width: 24px;
+  height: 24px;
 `;
