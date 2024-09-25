c5fd7c7acdc7ac600d144fbcd6dc7e6e5c2ef1bf
HAE2NI <hae0_02ni@ewhain.net>
2024-09-25 16:57:29 +0900

    💄 design: add commonStyle about svg Icon wrapper

--- ceos/src/style/commonStyle.ts
+++ ceos/src/style/commonStyle.ts
@@ -1,3 +1,4 @@
+import { FunctionComponent, SVGProps } from "react";
 import styled, { css } from "styled-components";
 
 export const rowFlex = css`
@@ -12,8 +13,18 @@ export const columnFlex = css`
   flex-direction:column
 `;
 
-export const Icon = styled.button`
+type SVGComponent = FunctionComponent<SVGProps<SVGSVGElement>>;
+
+export const Icon = (Component: SVGComponent) => styled(Component)`
   cursor: pointer;
   width: 24px;
   height: 24px;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 343px;
 `;
