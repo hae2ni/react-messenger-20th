894b63ca487b40accf6456de2535aeeca294e802
HAE2NI <hae0_02ni@ewhain.net>
2024-09-25 15:53:03 +0900

    📦 pack: svg component화

--- ceos/vite.config.ts
+++ ceos/vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr({
+      include: "**/*.svg?react",
+    }),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "src") },
