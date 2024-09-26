2223034c3496e78fb50f0da3787e42cbbfd46b9d
HAE2NI <hae0_02ni@ewhain.net>
2024-09-26 16:42:44 +0900

    💄 design: ADD ToMessageBox FLEX

--- ceos/src/components/message/ToBox.tsx
+++ ceos/src/components/message/ToBox.tsx
@@ -1,12 +1,18 @@
+import styled from "styled-components";
 import MessageBox from "./MessageBox";
 import { chatType } from "types/chatType";
 import MessageTime from "./MessageTime";
 
 export default function ToBox({ id, message, time }: chatType) {
   return (
-    <>
+    <Wrapper>
       <MessageTime time={time} />
       <MessageBox id={id} text={message} />
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
