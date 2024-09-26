d93cbb21bce330f15b48fb334619e5c1dfed6b35
HAE2NI <hae0_02ni@ewhain.net>
2024-09-26 13:30:10 +0900

    ✨ feat: inputRef 추가

--- ceos/src/components/ChattingInput.tsx
+++ ceos/src/components/ChattingInput.tsx
@@ -5,16 +5,46 @@ import PlusIC from "@/assets/icons/plus_ic.svg?react";
 import SmileIC from "@/assets/icons/smile_ic.svg?react";
 import HashtagIC from "@/assets/icons/hashtag_ic.svg?react";
 
+import { useRef } from "react";
+import { useStore } from "@core/useStore";
+
 export default function ChattingInput() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const setInputText = useStore((state) => state.setInputValue);
+  const setDummyText = useStore((state) => state.setDummyText);
+
+  function handleText() {
+    if (inputRef.current) {
+      const enteredText = inputRef.current.value;
+      setInputText(enteredText);
+      setDummyText();
+      inputRef.current.value = "";
+    }
+  }
+
+  function handlePressEnter(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key == "Enter") {
+      e.preventDefault();
+      handleText();
+    }
+  }
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    handleText();
+  }
+
   return (
     <Wrapper>
       <ContentContainer>
         <PlusIcon />
-        <Input autoFocus />
-        <RightWrapper>
-          <SmileICon />
-          <HashtagICon />
-        </RightWrapper>
+        <Form onSubmit={handleSubmit}>
+          <Input onKeyDown={handlePressEnter} type="text" ref={inputRef} autoFocus={true} />
+          <RightWrapper>
+            <SmileICon />
+            <HashtagICon onClick={handleText} />
+          </RightWrapper>
+        </Form>
       </ContentContainer>
     </Wrapper>
   );
@@ -35,6 +65,10 @@ const RightWrapper = styled.div`
 const PlusIcon = Icon(PlusIC);
 const SmileICon = Icon(SmileIC);
 const HashtagICon = Icon(HashtagIC);
+
+const Form = styled.form`
+  display: flex;
+`;
 
 const Input = styled.input`
   ${rowFlex}
