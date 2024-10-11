ae95cb7e67c56f2d0c3b11574e02ca1262893576
HAE2NI <hae0_02ni@ewhain.net>
2024-10-11 22:46:15 +0900

    etc: add friend data

---
+++ ceos/src/constant/friends.ts
@@ -0,0 +1,22 @@
+import { friendListType } from "types/friendListType";
+
+export const FRIEND_LIST: friendListType[] = [
+  {
+    id: "red",
+    user: "빨강이",
+    statusMessage: "나는 빨간색",
+    profile: "https://i.pinimg.com/474x/50/f5/41/50f5413e122f31301f3f8ba356db55b5.jpg",
+  },
+  {
+    id: "yellow",
+    user: "노랑이",
+    statusMessage: "나는 노란색",
+    profile: "https://i.pinimg.com/enabled_hi/474x/bf/e9/7c/bfe97ce7c40a81cd4739ebbf0333e080.jpg",
+  },
+  {
+    id: "blue",
+    user: "파랑이",
+    statusMessage: "나는 파란색",
+    profile: "https://i.pinimg.com/474x/40/e2/3a/40e23a7b0a9da5431f9929830d3fccba.jpg",
+  },
+];
