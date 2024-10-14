7c4ab9bf684288ab5ee2e3b8381daea6c4a0d33b
HAE2NI <hae0_02ni@ewhain.net>
2024-10-14 13:41:55 +0900

    🚚 change file import path

--- ceos/src/pages/Home.tsx
+++ ceos/src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import Favorites from "@components/friendList/Favorites";
+import Favorites from "@components/friendList/FavFriends/Favorites";
 import FriendList from "@components/friendList/FriendList";
-import Header from "@components/friendList/Header";
+import Header from "@components/common/Header";
 import MyProfile from "@components/friendList/MyProfile";
 
 export default function Home() {
