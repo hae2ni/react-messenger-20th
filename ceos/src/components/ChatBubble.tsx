eec256161bcc5cf5a026aaaa7d9fd51ff8ea892b
HAE2NI <hae0_02ni@ewhain.net>
2024-10-30 15:59:14 +0900

    ✨ feat: make chatbubble to follow useParams

--- ceos/src/components/ChatBubble.tsx
+++ ceos/src/components/ChatBubble.tsx
@@ -4,27 +4,32 @@ import FromBox from "./message/FromBox";
 import { useStore } from "@core/useStore";
 import ToBox from "./message/ToBox";
 
-export default function ChatBubble() {
+export default function ChatBubble({ userId }) {
   const newDummyText = useStore((state) => state.dummyText);
 
+  const friendData = newDummyText.find((friend) => friend.userId === userId);
+  if (!friendData) {
+    return console.log("no..");
+  }
+  console.log(friendData.messages);
   return (
     <>
-      {newDummyText.map((data, index, array) => {
-        const { id, message, time } = data;
+      {friendData.messages.map((data, index, array) => {
+        const { sender, text, time } = data;
 
         const prevMessage = array[index - 1];
         const nextMessage = array[index + 1];
 
         //다음 메세지랑 동시에 엔터엔터,,,를 했냐마냐
-        const isSameTime = nextMessage?.id === id && nextMessage?.time === time;
+        const isSameTime = nextMessage?.sender === sender && nextMessage?.time === time;
 
         //받은 메세지고, 전에랑 time이 달라짐!
-        const showProfile = index === 0 || prevMessage?.id !== id || prevMessage?.time !== time;
+        const showProfile = index === 0 || prevMessage?.sender !== sender || prevMessage?.time !== time;
 
-        //id가 같고, time이 앞에 메세지랑 같지만, 그 시간에 그 사람이 마지막으로 보낸 메세지임
-        const lastMessage = prevMessage?.id === id && prevMessage?.time === time && nextMessage?.time != time;
+        //user가 같고, time이 앞에 메세지랑 같지만, 그 시간에 그 사람이 마지막으로 보낸 메세지임
+        const lastMessage = prevMessage?.sender === sender && prevMessage?.time === time && nextMessage?.time != time;
 
-        if (id != "나") {
+        if (sender != "me") {
           return (
             <FromContainer key={Math.random()}>
               {showProfile && <Profile />}
@@ -32,8 +37,8 @@ export default function ChatBubble() {
                 lastMessage={lastMessage}
                 isSameTime={isSameTime}
                 showProfile={showProfile}
-                id={id}
-                message={message}
+                user={sender}
+                message={text}
                 time={time}
               />
             </FromContainer>
@@ -41,7 +46,7 @@ export default function ChatBubble() {
         } else {
           return (
             <ToWrapper key={Math.random()}>
-              <ToBox showProfile={showProfile} message={message} time={time} />
+              <ToBox showProfile={showProfile} message={text} time={time} />
             </ToWrapper>
           );
         }
@@ -51,18 +56,21 @@ export default function ChatBubble() {
 }
 
 const Profile = styled(ProfileImg)`
-  width: 48px;
   height: 48px;
+
+  wuserth: 48px;
 `;
 
 const FromContainer = styled.div`
   display: flex;
-  width: 100%;
   padding-left: 10px;
+
+  wuserth: 100%;
 `;
 
 const ToWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
-  width: 100%;
+
+  wuserth: 100%;
 `;
