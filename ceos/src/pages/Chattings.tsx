2b8be33ca7478b4cab16e5001c947ae2b00964ed
HAE2NI <hae0_02ni@ewhain.net>
2024-10-30 17:39:05 +0900

    💄 add ui

--- ceos/src/pages/Chattings.tsx
+++ ceos/src/pages/Chattings.tsx
@@ -1,3 +1,16 @@
+import List from "@components/chattings/List";
+import Header from "@components/common/Header";
+import styled from "styled-components";
+
 export default function Chattings() {
-  return <div>Chattings</div>;
+  return (
+    <Content>
+      <Header />
+      <List />
+    </Content>
+  );
 }
+
+const Content = styled.div`
+  margin: 0 16px;
+`;
--- ceos/src/pages/Home.tsx
+++ ceos/src/pages/Home.tsx
@@ -3,13 +3,19 @@ import FriendList from "@components/friendList/FriendList";
 import Header from "@components/common/Header";
 import MyProfile from "@components/friendList/MyProfile";
 
+import styled from "styled-components";
+
 export default function Home() {
   return (
-    <>
+    <Content>
       <Header />
       <MyProfile />
       <Favorites />
       <FriendList />
-    </>
+    </Content>
   );
 }
+
+const Content = styled.div`
+  margin: 0 16px;
+`;
