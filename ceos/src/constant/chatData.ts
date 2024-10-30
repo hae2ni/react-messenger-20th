8a2c0eb9dd69ec2dfe13da6b279095d01815c7b7
HAE2NI <hae0_02ni@ewhain.net>
2024-10-30 17:31:13 +0900

    🎨

--- ceos/src/constant/chatData.ts
+++ ceos/src/constant/chatData.ts
@@ -3,7 +3,7 @@ export const CHAT_DATA = [
     userId: "red",
     user: "빨강이",
     messages: [
-      { sender: "friend1", text: "안녕!", time: "2024-10-30T10:00:00Z" },
+      { sender: "빨강이", text: "안녕!", time: "2024-10-30T10:00:00Z" },
       { sender: "me", text: "안녕, 잘 지내?", time: "2024-10-30T10:05:00Z" },
     ],
   },
@@ -11,7 +11,7 @@ export const CHAT_DATA = [
     userId: "yellow",
     user: "노랑이",
     messages: [
-      { sender: "yellow", text: "안녕!", time: "2024-10-30T10:00:00Z" },
+      { sender: "노랑이", text: "안녕!", time: "2024-10-30T10:00:00Z" },
       { sender: "me", text: "안녕, 잘 지내?", time: "2024-10-30T10:05:00Z" },
     ],
   },
