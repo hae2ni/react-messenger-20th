b24c719661413f338bb1f3f1ca6c34120d14b277
HAE2NI <hae0_02ni@ewhain.net>
2024-10-30 17:38:35 +0900

    ✨ feat: useLocalStorage

--- ceos/src/core/useStore.ts
+++ ceos/src/core/useStore.ts
@@ -1,41 +1,56 @@
 import { CHAT_DATA } from "constant/chatData";
 import { chatType } from "types/chatType";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface StoreState {
   inputValue: string;
   setInputValue: (text: string) => void;
-  setDummyText: () => void;
   dummyText: chatType[];
   addNewText: (userId: string) => void;
 }
 
-export const useStore = create<StoreState>((set) => ({
-  dummyText: CHAT_DATA,
-  inputValue: "",
+interface StoreState {
+  inputValue: string;
+  setInputValue: (text: string) => void;
+  dummyText: chatType[];
+  addNewText: (userId: string) => void;
+}
 
-  setInputValue: (text) => set({ inputValue: text }),
+export const useStore = create<StoreState>()(
+  persist(
+    (set) => ({
+      dummyText: CHAT_DATA,
+      inputValue: "",
 
-  addNewText: (userId) =>
-    set((state) => {
-      const newText = {
-        sender: "me",
-        text: state.inputValue,
-        time: new Date().toISOString(),
-      };
+      setInputValue: (text) => set({ inputValue: text }),
 
-      const updatedDummyText = state.dummyText.map((friend) =>
-        friend.userId === userId
-          ? {
-              ...friend,
-              messages: [...friend.messages, newText],
-            }
-          : friend,
-      );
+      addNewText: (userId) =>
+        set((state) => {
+          const newText = {
+            sender: "me",
+            text: state.inputValue,
+            time: new Date().toISOString(),
+          };
 
-      return {
-        dummyText: updatedDummyText,
-        inputValue: "",
-      };
+          const updatedDummyText = state.dummyText.map((friend) =>
+            friend.userId === userId
+              ? {
+                  ...friend,
+                  messages: [...friend.messages, newText],
+                }
+              : friend,
+          );
+
+          return {
+            dummyText: updatedDummyText,
+            inputValue: "",
+          };
+        }),
     }),
-}));
+    {
+      name: "chat-storage",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
